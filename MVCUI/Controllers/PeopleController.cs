e08fa20f2131fc832c74e71e941284ee07e85465
Niko Makridakis <nmak345@gmail.com>
2020-05-01 06:38:33 -0500

    Added create person page

---
+++ MVCUI/Controllers/PeopleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            List<PersonModel> availablePeople = GlobalConfig.Connection.GetPerson_All();
+            return View(availablePeople);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                PersonModel p = new PersonModel();
+                p.FirstName = collection["FirstName"];
+                p.LastName = collection["LastName"];
+                p.EmailAddress = collection["EmailAddress"];
+                p.CellphoneNumber = collection["CellphoneNumber"];
+
+                GlobalConfig.Connection.CreatePerson(p);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
