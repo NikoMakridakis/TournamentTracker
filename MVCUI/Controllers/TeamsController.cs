f0c762d3a5440f1e378d9f4aa6f6f5e6aeec6e46
Niko Makridakis <nmak345@gmail.com>
2020-05-04 07:48:49 -0500

    Added create tournament page

--- MVCUI/Controllers/TeamsController.cs
+++ MVCUI/Controllers/TeamsController.cs
@@ -49,7 +49,6 @@ namespace MVCUI.Controllers
                 {
                     return RedirectToAction("Create");
                 }
-                
             }
             catch
             {
